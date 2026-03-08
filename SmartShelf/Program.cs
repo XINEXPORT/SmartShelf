c59b1513ba6a27b9f00721fdece6d7b69682701b
choang03 <choang03@EE144-03>
2026-03-08 19:00:40 -0400

    Add project files.

---
+++ SmartShelf/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+// Reference the API
+using ThingMagic;
+// for Thread.Sleep
+using System.Threading;
+using System.Runtime.Remoting.Messaging;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Reader r = null;
+        r = Reader.Create("");
+
+        //Uncomment this line to add default transport listener.
+        r.Transport += r.SimpleTransportListener;
+        int[] antennaList = { 1 };
+        r.Connect();
+        TagReadData[] tagReads;
+        r.ParamSet("/reader/region/id", Reader.Region.NA);
+        r.ParamSet("/reader/radio/readPower", 2000);
+
+        // Create a simplereadplan which uses the antenna list created above
+        SimpleReadPlan plan = new SimpleReadPlan(antennaList, TagProtocol.GEN2, null, null, 1000);
+
+        // Set the created readplan
+        r.ParamSet("/reader/read/plan", plan);
+
+        // Read tags
+        tagReads = r.Read(250);
+
+        // Process tags
+        foreach (TagReadData tr in tagReads)
+        {
+            Console.WriteLine(tr.EpcString);
+            int x = 1;
+        }
+        // added this to close the port and end program
+        r.Destroy();
+    }
+}
