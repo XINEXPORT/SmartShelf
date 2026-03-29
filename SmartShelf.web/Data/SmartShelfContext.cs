8ae331345369580b417d3eadef35f2ce1128fffd
XINEXPORT <christinehoangt@gmail.com>
2026-03-28 19:24:04 -0500

    Included EFCore in Program.cs and added the SmartShelf context required to connext to the database. Added a data folder where the SmartShelf dbcontext will be stored. It is called SmartShelfContext.cs

---
+++ SmartShelf.web/Data/SmartShelfContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SmartShelf.web.Data
+{
+    public class SmartShelfContext : DbContext
+    {
+        public SmartShelfContext(DbContextOptions<SmartShelfContext> options)
+            : base(options) { }
+    }
+}
