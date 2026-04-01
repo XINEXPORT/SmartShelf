703e094b2214cfe95af13c450e2bee4c3bad8d0e
choang03 <choang03@EE144-03>
2026-03-31 22:06:14 -0400

    adding product entity

--- SmartShelf.web/Models/Product.cs
+++ SmartShelf.web/Models/Product.cs
@@ -1,6 +1,19 @@
-﻿namespace SmartShelf.web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartShelf.web.Models
 {
     public class Product
     {
+        [Key]
+        public int Id { get; set; }  
+
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public int Threshold { get; set; }  
+
+        // one product → many tags
+        public ICollection<Tag> Tags { get; set; } = new List<Tag>();
     }
 }
