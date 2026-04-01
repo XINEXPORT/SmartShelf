f5a6a78ccd47254d3706c8341025f84b50c34bcd
choang03 <choang03@EE144-03>
2026-03-31 21:45:32 -0400

    adding tag entity

--- SmartShelf.web/Models/Tag.cs
+++ SmartShelf.web/Models/Tag.cs
@@ -1,6 +1,20 @@
-﻿namespace SmartShelf.web.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SmartShelf.web.Models
 {
     public class Tag
     {
+        [Key]
+        public string EPC { get; set; } = null!;  
+
+        [Required]
+        public int ProductId { get; set; }        
+
+        [ForeignKey(nameof(ProductId))]
+        public Product Product { get; set; } = null!;
+
+        // one tag → many read events
+        public ICollection<TagReadEvent> TagReadEvents { get; set; } = new List<TagReadEvent>();
     }
 }
